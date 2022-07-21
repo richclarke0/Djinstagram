6ec9c2196cdc749bd6a39afacd2cb8fe7b4f5811
Ramon Velarde <velardeglaser@Ramons-Air.home>
2022-07-20 21:14:56 -0400

    models scratch

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,32 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
-class Post:
-    img = models.CharField(max_length=100)
-    desc = models.CharField(max_length=255)
-    #continue here
-    
+class User(models.Model):
+    name = models.CharField(max_length=175)
+    email = models.CharField(max_length=175)
+    profile_pic = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse(kwargs={'user_id': self.id})
+
+
+class Post(models.Model):
+    photo_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=250)
+    comments = ArrayField(
+        models.CharField(max_length=512))
+    favorite = ArrayField(
+        models.CharField(max_length=512))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+    # def get_absolute_url(self):
+    #     return reverse('detail', kwargs={'post_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, View
+from .models import Post
 
 posts = ""
 
 def home(request):
     return render(request, 'index.html', { 'posts' : posts })
+
+def post_index(request):
+    cats = Post.objects.all()
+    return render(request, 'post/index.html', { 'posts': posts })
