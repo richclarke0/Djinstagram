5308c185460eb79d3a9bb7ab4cb8393a4eb551da
Richard Clarke <freshnewrich@gmail.com>
2022-07-24 16:46:50 -0700

    add date to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -38,6 +38,7 @@ class Post(models.Model):
     # favorite = ArrayField(
     #     models.CharField(max_length=512))
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
  
     def __str__(self):
         return self.caption
