67e8e37c81db17afd1e49858f80c310ff4a6e0b9
Ben <ben.a.burgos@gmail.com>
2022-07-24 17:24:32 -0700

    reorganizes paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('posts/create/', views.PostCreate.as_view(), name='post_create'),
     path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
     path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
-    path('accounts/signup/', views.signup, name='signup'),
     path('posts/<int:post_id>/<int:user_id>/add_comment/', views.add_comment, name="add_comment"),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
